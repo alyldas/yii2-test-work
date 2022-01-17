31d8e3eabd4bafc140cfcc9bb9ac0163234b4795
alyldas <alyldas@ya.ru>
2022-01-17 19:05:35 +0300

    Added User fixtures
    `yii fixture/load User`

--- config/console.php
+++ config/console.php
@@ -28,13 +28,11 @@ $config = [
         'db' => $db,
     ],
     'params' => $params,
-    /*
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
-            'class' => 'yii\faker\FixtureController',
+            'class' => 'yii\console\controllers\FixtureController',
         ],
     ],
-    */
 ];
 
 if (YII_ENV_DEV) {
---
+++ tests/unit/fixtures/data/user.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+        'username' => 'user1',
+        'password_hash' => '$2y$13$HdWT6cnoHLNGXzNMaTH23ehMA34lsPAuqErB1XvvNW7GIgyaGe7Sm', // password
+        'auth_key' => 'K3nF70it7tzNsHddEiq0BZ0ibOU8S3xV',
+        'created_at' => 1642433428,
+        'updated_at' => 1642433428,
+    ],
+    [
+        'username' => 'user2',
+        'password_hash' => '$2y$13$HdWT6cnoHLNGXzNMaTH23ehMA34lsPAuqErB1XvvNW7GIgyaGe7Sm', // password
+        'auth_key' => 'dZlXsVnIDgIzFgX4EduAqkEPuOhhOh9q',
+        'created_at' => 1642433428,
+        'updated_at' => 1642433428,
+    ],
+];
